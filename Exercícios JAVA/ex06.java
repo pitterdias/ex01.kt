ef0d66654f98e51df955654d160e53e0f087e553
PITTER VERDIANO DIAS <pitter.dias@escola.pr.gov.br>
2026-09-24 15:00:07 +0000

    dfdf

---
+++ Exercícios JAVA/ex06.java
@@ -0,0 +1,18 @@
+public class Exercicio06 {
+    public static void main(String[] args) {
+        String nome = "Ana";
+        int idade = 17;
+        String turma = "3DS";
+
+        System.out.println("APRESENTAÇÃO PESSOAL");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Turma: " + turma);
+    }
+}
+```[cite: 1]
+
+### Informações adicionais da tela:
+* **Documento:** Lista de exercícios 3º Trimestre[cite: 1]
+* **Pasta indicada:** Exercícios JAVA[cite: 1]
+* **Nome do arquivo:** `ex06.java`[cite: 1]
